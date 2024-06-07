cb7ed6818944167419b568ac15fe45f465fad9d9
willicassi <146377084+willicassi@users.noreply.github.com>
2024-06-07 13:50:10 -0700

    edit comments
    edit outdated comment

--- add_author.js
+++ add_author.js
@@ -51,7 +51,7 @@ addPersonForm.addEventListener("submit", function (e) {
 
 
 // Creates a single row from an Object representing a single record from 
-// bsg_people
+// authors-table
 addRowToTable = (data) => {
 
     // Get a reference to the current table on the page and clear it out.
